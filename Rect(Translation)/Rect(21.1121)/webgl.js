a2bb7249170d02da76c15f2e949096f8eb33cba2
ByeonggakYu <qudrkr0815@korea.ac.kr>
2021-12-07 16:09:18 +0900

    [practice]

--- Rect(Translation)/Rect(21.1121)/webgl.js
+++ Rect(Translation)/Rect(21.1121)/webgl.js
@@ -1,7 +1,3 @@
-
-
-console.log("init!");
-
 window.onload = init();
 
 function init() {
@@ -11,6 +7,7 @@ function init() {
   canvas.width = 500;
   canvas.height = 500;
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.enable(gl.DEPTH_TEST);
   const program = createProgram(gl);
   draw(canvas,gl, program);
 }
@@ -48,15 +45,78 @@ function createProgram(gl) {
 
 function draw(canvas, gl, program) {
     let triangleVertex = [
-        // X Y Z              R G B
-        0.0, 0.5, 0.0,      1.0, 1.0, 0.0,
-        -0.5, -0.5, 0.0,    0.7, 0.0, 1.0,
-        0.5, -0.5, 0.0,     0.1, 1.0, 0.6
+        // Top
+        -1.0, 1.0, -1.0, 0.5,0.5,0.5,
+        -1.0, 1.0, 1.0, 0.5,0.5,0.5,
+        1.0, 1.0, 1.0, 0.5,0.5,0.5,
+        1.0, 1.0, -1.0, 0.5,0.5,0.5,
+
+        //Left
+        -1.0, 1.0, 1.0, 0.75,0.75,0.5,
+        -1.0, -1.0, 1.0, 0.75,0.75,0.5,
+        -1.0, -1.0, -1.0, 0.75,0.75,0.5,
+        -1.0, 1.0, -1.0, 0.75,0.75,0.5,
+
+        //Right
+        1.0, 1.0, 1.0, 0.25,0.25,0.75,
+        1.0, -1.0, 1.0, 0.25,0.25,0.75,
+        1.0, -1.0, -1.0, 0.25,0.25,0.75,
+        1.0, 1.0, -1.0, 0.25,0.25,0.75,
+
+        // Front
+        1.0, 1.0, 1.0, 1.0,0.25,0.75,
+        1.0, -1.0, 1.0, 1.0,0.25,0.75,
+        -1.0, -1.0, 1.0, 1.0,0.25,0.75,
+        -1.0, 1.0, 1.0, 1.0,0.25,0.75,
+
+        // Back
+        1.0, 1.0, -1.0, 0.0,1.0,0.55,
+        1.0, -1.0, -1.0, 0.0,1.0,0.55,
+        -1.0, -1.0, -1.0, 0.0,1.0,0.55,
+        -1.0, 1.0, -1.0, 0.0,1.0,0.55,
+
+        // Bottom
+        -1.0, -1.0, -1.0, 0.5,0.5,1.0,
+        -1.0, -1.0, 1.0, 0.5,0.5,1.0,
+        1.0, -1.0, 1.0, 0.5,0.5,1.0,
+        1.0, -1.0, -1.0, 0.5,0.5,1.0,
     ];
 
-    let triangleVertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBuffer);
+    let boxIndices = 
+    [
+        //Top
+        0,1,2,
+        0,2,3,
+
+        //Left
+        5,4,6,
+        6,4,7,
+
+        //Right
+        8,9,10,
+        8,10,11,
+
+        //Front
+        13,12,14,
+        15,14,12,
+
+        //Back
+        16,17,18,
+        16,18,19,
+
+        //Bottom
+        21,20,22,
+        22,20,23
+    ];
+
+    let boxVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertex), gl.STATIC_DRAW);
+
+    let boxIndexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
+
 
     let vertexPosLocation = gl.getAttribLocation(program, 'a_position');
     let colorLocation = gl.getAttribLocation(program, 'a_color');
@@ -87,7 +147,7 @@ function draw(canvas, gl, program) {
     let matProjUniformLocation = gl.getUniformLocation(program,'mProj');
 
     let worldMatrix = mat4();
-    let viewMatrix = lookAt([0,0,-3], [0,0,0], [0,1,0]); // view matrix 를 설정해도, projmatrix 를 설정하지 않으면 소용이 없다. 
+    let viewMatrix = lookAt([0,0,-8], [0,0,0], [0,1,0]); // view matrix 를 설정해도, projmatrix 를 설정하지 않으면 소용이 없다. 
     let projMatrix = perspective(45, canvas.width / canvas.height, 0.1, 1000.0);
 
 
@@ -100,11 +160,13 @@ function draw(canvas, gl, program) {
     let angle = 0;
     let loop = function() {
         angle = performance.now() / 15 / 6 * 2 * Math.PI;
-        worldMatrix = rotate(angle, [0,1,0]);
+        yRotateMatrix = rotate(angle, [0,1,0]);
+        xRotateMatrix = rotate(angle/6, [1,0,0]);
+        worldMatrix = mult(yRotateMatrix, xRotateMatrix);
         gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, flatten(worldMatrix));
         gl.clearColor(0.75, 0.85, 0.8, 1.0);
         gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
         requestAnimationFrame(loop);
     }
     requestAnimationFrame(loop);
